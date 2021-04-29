d2894c6ee2731eb8ae35ab50a1a5df145091fb1a
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2021-04-30 03:50:54 +0430

    make backup

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'avatar', 'description']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content']
+    list_display = ['title', 'category', 'created_at']
+
+
+admin.site.register(Article, ArticleAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'cover']
+
+
+admin.site.register(Category, CategoryAdmin)
