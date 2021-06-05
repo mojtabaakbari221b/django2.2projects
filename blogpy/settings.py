0029543eb223ab4d7fd1d8d48a615d7364ea18ed
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2021-06-05 14:03:52 +0430

    update project

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -122,4 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, 'static'),
+]
+
+# Media Settings
+MEDIA_URL = 'files/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
